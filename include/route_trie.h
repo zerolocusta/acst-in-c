e9ebc81ce6887900364a29b0c6b7144769f437bc
zerolocusta <zerolocust@gmail.com>
2017-01-21 22:58:37 +0800

    still working on route_trie

--- include/route_trie.h
+++ include/route_trie.h
@@ -1,9 +1,14 @@
+#ifndef ACST_ROUTE_TRIE_H
+#define ACST_ROUTE_TRIE_H
 #include <regex.h>
 #include "mongoose.h"
 
+#define find_char_index(ch) ((ch) - '!') 
+
 typedef struct _rtrie_node{
     const char *uri;
-    (sturct _rtrie_node *) next[94];
+    /* start from '!' to '~'*/
+    sturct _rtrie_node *p[94];
     regex_t regex;
     mg_event_handler_t event_handler;
 } rtire_node_t;
@@ -15,3 +20,5 @@ typedef struct _rtrie{
 
 int trie_init(rtrie_t *);
 int add_route(rtrie_t *, const char *);
+
+#endif
--- src/route_trie.c
+++ src/route_trie.c
@@ -1,23 +1,49 @@
 #include <strings.h>
 #include "route_trie.h"
 
+trie_node_t *trie_node_create(){
+    rtrie_node_t *rtn = (rtrie_node_t *) malloc(sizeof(rtrie_node_t));
+    if(rtn == NULL)
+        return rtn;
+    bzero(rtn, sizeof(rtire_node_t));
+    for(int i = 0; i < 94; i++)
+        rtn->p[i] = NULL;
+    return rtn;
+}
+
 int trie_init(rtrie_t *rt)
 {
-    rtrie_node_t *root = (rtrie_node_t *) malloc(sizeof(rtrie_node_t));
-    bzero(root, sizeof(rtire_node_t));
+    rtrie_node_t *root = trie_node_create();
     if (root = NULL)
         return -1;
     rt->root = root;
     return 0;
 }
 
+regex_t parse_uri_regex(const struct mg_str *uri)
+{
+    
+}
 
-
-int add_route(rtrie_t *rt, const char *uri)
+int add_route(rtrie_t *rt, const struct mg_str *uri)
 {
     if (rt->root == NULL)
     {
         if (trie_init(rt) < 0)
             return -1;
+    }
+    rtrie_node_t *next = rt->root;
+    size_t i = 0;
+    while(i < uri->len)
+    {
+        size_t ch_index = (size_t) find_char_index( (uri->p)[i] );
+        if (next->p[ch_index] == NULL)
+        {
+            next->p[ch_index] = trie_node_create();
+            if(next->p[ch_index] == NULL)
+                return -1;
+        }
+        i++;
+        next = next->p[ch_index];
     }
 }
